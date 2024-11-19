a1a0b3e10c82f857353ed514c21dc9dbbbe0a212
Prime-Property <vishalpardeshi8830@gmail.com>
2024-11-19 09:25:06 +0530

    added the increament

---
+++ IncrementCode/IncrementCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IncrementCode
+{
+    public class Program
+    {
+       static string NewCandidateCode;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the code:");
+            string code= Console.ReadLine();
+            Console.WriteLine(dothingincrement(code));
+            Console.ReadLine();
+        }
+        public static string dothingincrement(string CandidateCode)
+        {
+            string characters = Regex.Match(CandidateCode, @"^[A-Za-z]+").Value;
+
+            string digitsString = Regex.Match(CandidateCode, @"\d+$").Value;
+            int digits = int.Parse(digitsString);
+
+            int NewCount = digits + 1;
+
+            NewCandidateCode = characters + NewCount.ToString();
+            return NewCandidateCode;
+        }
+    }
+}
